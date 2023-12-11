7e5ca43e944db30c7f99091013bbeb4754d766b8
Richard Purdie <richard.purdie@linuxfoundation.org>
2023-12-11 17:35:53 +0000

    bitbake.conf/pseudo: Switch from exclusion list to inclusion list
    Currently, pseudo tracks all files referenced within it's presence unless
    they're listed in an exclusion list. The exclusion list has grown to be
    fairly unwieldy.
    
    This patch swaps PSEUDO_IGNORE_PATHS for PSEUDO_INCLUDE_PATHS which in
    theory should be easier and more explicit to maintain.
    
    (From OE-Core rev: 88a127e957077562822c30004d980a408f930b4c)
    
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/lib/oeqa/selftest/cases/runtime_test.py
+++ meta/lib/oeqa/selftest/cases/runtime_test.py
@@ -166,16 +166,19 @@ TEST_RUNQEMUPARAMS += " slirp"
 
         bitbake('gnupg-native -c addto_recipe_sysroot')
 
+        # gpg requires really short paths
+        homedir = get_bb_var('HOME')
+        native_sysroot = get_bb_var("RECIPE_SYSROOT_NATIVE", "gnupg-native")
+
         # Enable package feed signing
-        self.gpg_home = tempfile.mkdtemp(prefix="oeqa-feed-sign-")
+        self.gpg_home = tempfile.mkdtemp(prefix="oeqa-feed-sign-", dir=homedir)
         self.track_for_cleanup(self.gpg_home)
         signing_key_dir = os.path.join(self.testlayer_path, 'files', 'signing')
-        runCmd('gpgconf --list-dirs --homedir %s; gpg -v --batch --homedir %s --import %s' % (self.gpg_home, self.gpg_home, os.path.join(signing_key_dir, 'key.secret')), native_sysroot=get_bb_var("RECIPE_SYSROOT_NATIVE", "gnupg-native"), shell=True)
+        runCmd('gpgconf --list-dirs --homedir %s; gpg -v --batch --homedir %s --import %s' % (self.gpg_home, self.gpg_home, os.path.join(signing_key_dir, 'key.secret')), native_sysroot=native_sysroot, shell=True)
         features += 'INHERIT += "sign_package_feed"\n'
         features += 'PACKAGE_FEED_GPG_NAME = "testuser"\n'
         features += 'PACKAGE_FEED_GPG_PASSPHRASE_FILE = "%s"\n' % os.path.join(signing_key_dir, 'key.passphrase')
         features += 'GPG_PATH = "%s"\n' % self.gpg_home
-        features += 'PSEUDO_IGNORE_PATHS .= ",%s"\n' % self.gpg_home
         self.write_config(features)
 
         bitbake('core-image-full-cmdline socat')
@@ -203,16 +206,19 @@ TEST_RUNQEMUPARAMS += " slirp"
 
         bitbake('gnupg-native -c addto_recipe_sysroot')
 
+        # gpg requires really short paths
+        homedir = get_bb_var('HOME')
+        native_sysroot = get_bb_var("RECIPE_SYSROOT_NATIVE", "gnupg-native")
+
         # Enable package feed signing
-        self.gpg_home = tempfile.mkdtemp(prefix="oeqa-feed-sign-")
+        self.gpg_home = tempfile.mkdtemp(prefix="oeqa-feed-sign-", dir=homedir)
         self.track_for_cleanup(self.gpg_home)
         signing_key_dir = os.path.join(self.testlayer_path, 'files', 'signing')
-        runCmd('gpgconf --list-dirs --homedir %s; gpg -v --batch --homedir %s --import %s' % (self.gpg_home, self.gpg_home, os.path.join(signing_key_dir, 'key.secret')), native_sysroot=get_bb_var("RECIPE_SYSROOT_NATIVE", "gnupg-native"), shell=True)
+        runCmd('gpgconf --list-dirs --homedir %s; gpg -v --batch --homedir %s --import %s' % (self.gpg_home, self.gpg_home, os.path.join(signing_key_dir, 'key.secret')), native_sysroot=native_sysroot, shell=True)
         features += 'INHERIT += "sign_package_feed"\n'
         features += 'PACKAGE_FEED_GPG_NAME = "testuser"\n'
         features += 'PACKAGE_FEED_GPG_PASSPHRASE_FILE = "%s"\n' % os.path.join(signing_key_dir, 'key.passphrase')
         features += 'GPG_PATH = "%s"\n' % self.gpg_home
-        features += 'PSEUDO_IGNORE_PATHS .= ",%s"\n' % self.gpg_home
         self.write_config(features)
 
         # Build core-image-sato and testimage
