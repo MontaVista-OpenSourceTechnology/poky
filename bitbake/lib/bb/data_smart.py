d25d9d4960208f85e2e077867f120241d2523b83
Jeremy A. Puhlman <jpuhlman@mvista.com>
2024-09-02 17:35:53 +0000

    Revert "bitbake: data_smart: Improve performance for VariableHistory"
    Source: MontaVista Software, LLC
    MR: 161761
    Type: Defect Fix
    Disposition: Local
    ChangeID: 9a1834c8a2cdd557ae20189524583f960812171b
    Description:
    
    Introduction of this patch causes a python backtace.
    Remove for now.
    
    This reverts commit 3b75a4f14161b8f7c509369c6b985b2c6a7faab1.
    
    Signed-off-by: Jeremy A. Puhlman <jpuhlman@mvista.com>

--- bitbake/lib/bb/data_smart.py
+++ bitbake/lib/bb/data_smart.py
@@ -272,9 +272,12 @@ class VariableHistory(object):
             return
         if 'op' not in loginfo or not loginfo['op']:
             loginfo['op'] = 'set'
+        if 'detail' in loginfo:
+            loginfo['detail'] = str(loginfo['detail'])
         if 'variable' not in loginfo or 'file' not in loginfo:
             raise ValueError("record() missing variable or file.")
         var = loginfo['variable']
+
         if var not in self.variables:
             self.variables[var] = []
         if not isinstance(self.variables[var], list):
@@ -333,8 +336,7 @@ class VariableHistory(object):
                     flag = '[%s] ' % (event['flag'])
                 else:
                     flag = ''
-                o.write("#   %s %s:%s%s\n#     %s\"%s\"\n" % \
-                    (event['op'], event['file'], event['line'], display_func, flag, re.sub('\n', '\n#     ', str(event['detail']))))
+                o.write("#   %s %s:%s%s\n#     %s\"%s\"\n" % (event['op'], event['file'], event['line'], display_func, flag, re.sub('\n', '\n#     ', event['detail'])))
             if len(history) > 1:
                 o.write("# pre-expansion value:\n")
                 o.write('#   "%s"\n' % (commentVal))
@@ -388,7 +390,7 @@ class VariableHistory(object):
             if isset and event['op'] == 'set?':
                 continue
             isset = True
-            items = d.expand(str(event['detail'])).split()
+            items = d.expand(event['detail']).split()
             for item in items:
                 # This is a little crude but is belt-and-braces to avoid us
                 # having to handle every possible operation type specifically
